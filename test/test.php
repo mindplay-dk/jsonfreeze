ac2440e9831617407ab01fa6747d1dd1e221f734
Rasmus Schultz <rasc@fynskemedier.dk>
2014-09-11 11:28:59 +0200

    reformat to PSR conventions

--- test/test.php
+++ test/test.php
@@ -3,35 +3,34 @@
 /**
  * This will eventually be a unit-test...
  */
-
 class Order
 {
-  public $orderNo;
-  protected $lines = array();
-  protected $paid = false;
-  
-  public function addLine(OrderLine $line)
-  {
-    $this->lines[] = $line;
-  }
-  
-  public function setPaid($paid = true)
-  {
-    $this->paid = true;
-  }
+    public $orderNo;
+    protected $lines = array();
+    protected $paid = false;
+
+    public function addLine(OrderLine $line)
+    {
+        $this->lines[] = $line;
+    }
+
+    public function setPaid($paid = true)
+    {
+        $this->paid = true;
+    }
 }
 
 class OrderLine
 {
-  public function __construct($item, $amount)
-  {
-    $this->item = $item;
-    $this->amount = $amount;
-  }
-  
-  public $item;
-  public $amount;
-  public $options;
+    public function __construct($item, $amount)
+    {
+        $this->item = $item;
+        $this->amount = $amount;
+    }
+
+    public $item;
+    public $amount;
+    public $options;
 }
 
 header('Content-type: text/plain');
@@ -45,7 +44,7 @@ use mindplay\jsonfreeze\JsonSerializer;
 $order = new Order;
 $order->orderNo = 123;
 $order->setPaid();
-$order->addLine( new OrderLine('milk "fuzz"', 3) );
+$order->addLine(new OrderLine('milk "fuzz"', 3));
 
 $cookies = new OrderLine('cookies', 7);
 $cookies->options = array('flavor' => 'chocolate', 'weight' => '1/2 lb');
@@ -58,7 +57,7 @@ $json = new JsonSerializer;
 
 $data = $json->serialize($order);
 
-echo "\n\n".$data;
+echo "\n\n" . $data;
 
 $unserialized = $json->unserialize($data);
 
@@ -72,7 +71,7 @@ $input = print_r($order, true);
 $output = print_r($unserialized, true);
 
 if ($output !== $input) {
-  echo "\nERROR: input and output do not match.\n\n";
+    echo "\nERROR: input and output do not match.\n\n";
 } else {
-  echo "\nVERIFIED: input object graph is identical to unserialized object graph.\n\n";
+    echo "\nVERIFIED: input object graph is identical to unserialized object graph.\n\n";
 }
