dbcb77f3547ce86b8b92446ed6fce2f1ebc886da
Rasmus Schultz <rasc@fynskemedier.dk>
2014-09-11 12:41:34 +0200

    basic unit test

--- test/test.php
+++ test/test.php
@@ -1,13 +1,27 @@
 <?php
 
-/**
- * This will eventually be a unit-test...
- */
+require '../mindplay/jsonfreeze/JsonSerializer.php';
+
+use mindplay\jsonfreeze\JsonSerializer;
+
+// TEST FIXTURES:
+
 class Order
 {
+    /**
+     * @var int
+     */
     public $orderNo;
-    protected $lines = array();
-    protected $paid = false;
+
+    /**
+     * @var OrderLine[]
+     */
+    public $lines = array();
+
+    /**
+     * @var bool
+     */
+    public $paid = false;
 
     public function addLine(OrderLine $line)
     {
@@ -22,6 +36,10 @@ class Order
 
 class OrderLine
 {
+    /**
+     * @param string $item
+     * @param int $amount
+     */
     public function __construct($item, $amount)
     {
         $this->item = $item;
@@ -30,48 +48,164 @@ class OrderLine
 
     public $item;
     public $amount;
-    public $options;
+    public $options = array();
 }
+
+// UNIT TEST:
 
 header('Content-type: text/plain');
 
-require '../mindplay/jsonfreeze/JsonSerializer.php';
+/**
+ * @return Order
+ */
+function get_order_fixture() {
+    $order = new Order;
+    $order->orderNo = 123;
+    $order->setPaid();
+    $order->addLine(new OrderLine('milk "fuzz"', 3));
 
-use mindplay\jsonfreeze\JsonSerializer;
+    $cookies = new OrderLine('cookies', 7);
+    $cookies->options = array('flavor' => 'chocolate', 'weight' => '1/2 lb');
+
+    $order->addLine($cookies);
+
+    return $order;
+}
 
 // Create a simple object graph:
 
-$order = new Order;
-$order->orderNo = 123;
-$order->setPaid();
-$order->addLine(new OrderLine('milk "fuzz"', 3));
+test(
+    'Can serialize and unserialize object graph',
+    function () {
+        $input = get_order_fixture();
 
-$cookies = new OrderLine('cookies', 7);
-$cookies->options = array('flavor' => 'chocolate', 'weight' => '1/2 lb');
+        $json = new JsonSerializer();
 
-$order->addLine($cookies);
+        $data = $json->serialize($input);
 
-// Serialize and unserialize:
+        /** @var Order $output */
+        $output = $json->unserialize($data);
 
-$json = new JsonSerializer;
+        eq($output->orderNo, $input->orderNo, 'object property value');
+        eq(array_keys($output->lines), array_keys($input->lines), 'array sequence');
+        eq($output->paid, $input->paid, 'scalar value');
 
-$data = $json->serialize($order);
+        eq($output->lines[0]->amount, $input->lines[0]->amount, 'nested value');
+        eq($output->lines[1]->options, $input->lines[1]->options, 'nested array');
+    }
+);
 
-echo "\n\n" . $data;
+// https://gist.github.com/mindplay-dk/4260582
 
-$unserialized = $json->unserialize($data);
+/**
+ * @param string   $name     test description
+ * @param callable $function test implementation
+ */
+function test($name, $function)
+{
+    echo "\n=== $name ===\n\n";
 
-echo "\n\nUnserialized:\n";
+    try {
+        call_user_func($function);
+    } catch (Exception $e) {
+        ok(false, "UNEXPECTED EXCEPTION", $e);
+    }
+}
 
-var_dump($unserialized);
+/**
+ * @param bool   $result result of assertion
+ * @param string $why    description of assertion
+ * @param mixed  $value  optional value (displays on failure)
+ */
+function ok($result, $why = null, $value = null)
+{
+    if ($result === true) {
+        echo "- PASS: " . ($why === null ? 'OK' : $why) . ($value === null ? '' : ' (' . format($value) . ')') . "\n";
+    } else {
+        echo "# FAIL: " . ($why === null ? 'ERROR' : $why) . ($value === null ? '' : ' - ' . format($value, true)) . "\n";
+        status(false);
+    }
+}
 
-// Validate serialize/unserialize accuracy:
+/**
+ * @param mixed  $value    value
+ * @param mixed  $expected expected value
+ * @param string $why      description of assertion
+ */
+function eq($value, $expected, $why = null)
+{
+    $result = $value === $expected;
 
-$input = print_r($order, true);
-$output = print_r($unserialized, true);
+    $info = $result
+        ? format($value)
+        : "expected: " . format($expected, true) . ", got: " . format($value, true);
 
-if ($output !== $input) {
-    echo "\nERROR: input and output do not match.\n\n";
-} else {
-    echo "\nVERIFIED: input object graph is identical to unserialized object graph.\n\n";
+    ok($result, ($why === null ? $info : "$why ($info)"));
+}
+
+/**
+ * @param string   $exception_type Exception type name
+ * @param string   $why            description of assertion
+ * @param callable $function       function expected to throw
+ */
+function expect($exception_type, $why, $function)
+{
+    try {
+        call_user_func($function);
+    } catch (Exception $e) {
+        if ($e instanceof $exception_type) {
+            ok(true, $why, $e);
+            return;
+        } else {
+            $actual_type = get_class($e);
+            ok(false, "$why (expected $exception_type but $actual_type was thrown)");
+            return;
+        }
+    }
+
+    ok(false, "$why (expected exception $exception_type was NOT thrown)");
+}
+
+/**
+ * @param mixed $value
+ * @param bool  $verbose
+ *
+ * @return string
+ */
+function format($value, $verbose = false)
+{
+    if ($value instanceof Exception) {
+        return get_class($value)
+        . ($verbose ? ": \"" . $value->getMessage() . "\"" : '');
+    }
+
+    if (! $verbose && is_array($value)) {
+        return 'array[' . count($value) . ']';
+    }
+
+    if (is_bool($value)) {
+        return $value ? 'TRUE' : 'FALSE';
+    }
+
+    if (is_object($value) && !$verbose) {
+        return get_class($value);
+    }
+
+    return print_r($value, true);
+}
+
+/**
+ * @param bool|null $status test status
+ *
+ * @return int number of failures
+ */
+function status($status = null)
+{
+    static $failures = 0;
+
+    if ($status === false) {
+        $failures += 1;
+    }
+
+    return $failures;
 }
