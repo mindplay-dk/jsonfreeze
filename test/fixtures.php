a8c94475b1d8ac9217324097104f5ab71b4e035c
Rasmus Schultz <rasmus@mindplay.dk>
2016-06-07 20:26:43 +0200

    clean up test-suite

---
+++ test/fixtures.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @return Order
+ */
+function get_order_fixture()
+{
+    $order = new Order;
+    $order->orderNo = 123;
+    $order->setPaid();
+    $order->addLine(new OrderLine('milk "fuzz"', 3));
+
+    $cookies = new OrderLine('cookies', 7);
+    $cookies->options = array('flavor' => 'chocolate', 'weight' => '1/2 lb');
+
+    $order->addLine($cookies);
+
+    return $order;
+}
+
+class Order
+{
+    /**
+     * @var int
+     */
+    public $orderNo;
+
+    /**
+     * @var OrderLine[]
+     */
+    public $lines = array();
+
+    /**
+     * @var bool
+     */
+    public $paid = false;
+
+    public function addLine(OrderLine $line)
+    {
+        $this->lines[] = $line;
+    }
+
+    public function setPaid($paid = true)
+    {
+        $this->paid = true;
+    }
+}
+
+class OrderLine
+{
+    /**
+     * @param string $item
+     * @param int    $amount
+     */
+    public function __construct($item, $amount)
+    {
+        $this->item = $item;
+        $this->amount = $amount;
+        $this->data = 456;
+    }
+
+    /**
+     * @var array this is here to assert omission of static properties
+     */
+    public static $cache = array();
+
+    /**
+     * @var int this is here to assert omission or inclusion of private properties
+     */
+    private $data = 123;
+
+    public $item;
+    public $amount;
+    public $options = array();
+}
